b524b8bc02ba2aabc2b3a343c6778fc83511448d
Skye <derpthulufhtagan@gmail.com>
2025-11-14 01:27:32 +0200

    fix white background gap in profile page top section

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -90,7 +90,7 @@ const ProfilePage = () => {
   return (
     <div className='flex min-h-screen flex-col'>
       {/* page background */}
-      <div className='flex-1 rounded-b-[16px] bg-white shadow-sm'>
+      <div className='rounded-b-[16px] bg-white shadow-sm'>
         {/* banner section */}
         <div className={`relative w-full bg-white ${isMobile ? 'h-52' : 'h-94'}`}>
           {/* TopBar  */}
